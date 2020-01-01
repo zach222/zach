6767178934939084d6ad424d8f2095f9f680cb21
zach222 <13275796507@163.com>
2020-01-01 15:40:58 +0800

    1-4Lombok

--- src/main/java/com/zach/springboot/controller/HelloController.java
+++ src/main/java/com/zach/springboot/controller/HelloController.java
@@ -1,15 +1,21 @@
 package com.zach.springboot.controller;
 
+import com.zach.springboot.pojo.Articl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
 
     @RequestMapping("/hello")
-    public String hellWorld( String name){
-        return "hello,world--"+name;
+    public Articl hellWorld( String name){
+
+        Articl articl = new Articl(1L,"zach");
+        log.info("测试代码"+ articl);
+        return articl;
 
     }
 }
--- src/main/java/com/zach/springboot/pojo/Articl.java
+++ src/main/java/com/zach/springboot/pojo/Articl.java
@@ -1,4 +1,15 @@
 package com.zach.springboot.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Articl {
+    Long id;
+    String name;
 }
