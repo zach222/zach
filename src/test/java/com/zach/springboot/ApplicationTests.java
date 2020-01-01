f1e64e1db1f4f483f00fef24ac125a2ff60ca1b9
zach222 <13275796507@163.com>
2020-01-01 15:19:41 +0800

    1-3hello world

---
+++ src/test/java/com/zach/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
